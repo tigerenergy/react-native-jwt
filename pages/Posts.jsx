f95bd5f5413ef530e363d4c7a791443b87ec67d8
kiwoon kim <kisoon0947@gmail.com>
2023-04-08 19:35:03 +0900

    업데이트

--- pages/Posts.jsx
+++ pages/Posts.jsx
@@ -1,43 +1,43 @@
-import { Alert, FlatList, Text, StyleSheet, View } from "react-native";
-import { Button, TextInput } from "react-native-paper";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { Alert, FlatList, Text, StyleSheet, View } from 'react-native'
+import { Button, TextInput } from 'react-native-paper'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export function Posts() {
-  const [cargando, setCargando] = useState(false);
-  const [post, setPost] = useState("");
-  const [posts, setPosts] = useState([]);
+  const [cargando, setCargando] = useState(false)
+  const [post, setPost] = useState('')
+  const [posts, setPosts] = useState([])
   useEffect(() => {
-    solicitarPosts();
-  }, []);
+    solicitarPosts()
+  }, [])
   async function solicitarPosts() {
-    setCargando(true);
+    setCargando(true)
     try {
-      const { data } = await axios.get("/posts");
-      setPosts(data);
+      const { data } = await axios.get('/posts')
+      setPosts(data)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-    setCargando(false);
+    setCargando(false)
   }
   async function createPost() {
-    setCargando(true);
+    setCargando(true)
     try {
-      const { data } = await axios.post("/posts", { content: post });
-      console.log(data);
+      const { data } = await axios.post('/posts', { content: post })
+      console.log(data)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-    setPost("");
-    solicitarPosts();
-    setCargando(false);
+    setPost('')
+    solicitarPosts()
+    setCargando(false)
   }
   return (
     <View style={styles.container}>
       <TextInput
         multiline
-        style={{ backgroundColor: "#f3f3f3" }}
-        label={"Post"}
+        style={{ backgroundColor: '#f3f3f3' }}
+        label={'Post'}
         value={post}
         onChangeText={setPost}
       />
@@ -52,13 +52,13 @@ export function Posts() {
         renderItem={({ item }) => <Text>{item.content}</Text>}
       />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     padding: 15,
   },
-});
+})
