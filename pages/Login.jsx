006851aa99df3d07edbb824a5fe688a0ea6ae59e
kiwoon kim <kisoon0947@gmail.com>
2023-04-08 19:35:26 +0900

    업데이트

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,24 +1,24 @@
-import { Text, StyleSheet, View } from "react-native";
-import { useContext } from "react";
-import { AuthContext } from "../context";
-import { Button } from "react-native-paper";
+import { Text, StyleSheet, View } from 'react-native'
+import { useContext } from 'react'
+import { AuthContext } from '../context'
+import { Button } from 'react-native-paper'
 
 export function Login({ navigation }) {
   function registro() {
-    navigation.replace("Registro");
+    navigation.replace('Registro')
   }
   return (
     <View style={styles.container}>
       <Button onPress={registro}>Registro</Button>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-});
+})
